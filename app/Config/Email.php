22cfc342d574f8315b402a40600bb64f1bb06000
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-06-18 21:33:37 +0530

    drop this idea for now with vuejs

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'sushilkumbhar@gmaill.com';
+    public string $fromName   = 'Sushil Kumbhar';
     public string $recipients = '';
 
     /**
